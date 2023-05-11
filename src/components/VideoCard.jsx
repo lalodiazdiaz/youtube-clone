10859cd5361ddc1479ced1dc1b8900ea62400156
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-05-10 18:18:08 -0600

    remove lines

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,11 +4,9 @@ import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
 import {
-  demoThumbnailUrl,
   demoVideoUrl,
   demoVideoTitle,
   demoChannelUrl,
-  demoChannelTitle,
 } from "../utils/constants";
 
 const VideoCard = ({
